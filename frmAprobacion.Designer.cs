4c26f8f3d31f6f2c1501c97c71fb3e9227ad9aac
jorge <jorge@RODAS>
2026-02-20 22:24:17 -0600

    frm aprobacion

--- frmAprobacion.Designer.cs
+++ frmAprobacion.Designer.cs
@@ -28,20 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.kryptonGroup1 = new Krypton.Toolkit.KryptonGroup();
+            this.label32 = new System.Windows.Forms.Label();
+            this.txtBuscar = new Krypton.Toolkit.KryptonTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAutorizar = new Krypton.Toolkit.KryptonButton();
+            this.btnNegar = new Krypton.Toolkit.KryptonButton();
+            this.kryptonDataGridView1 = new Krypton.Toolkit.KryptonDataGridView();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonGroup1.Panel)).BeginInit();
+            this.kryptonGroup1.Panel.SuspendLayout();
+            this.kryptonGroup1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonDataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // kryptonGroup1
+            // 
+            this.kryptonGroup1.Location = new System.Drawing.Point(41, 40);
+            // 
+            // kryptonGroup1.Panel
+            // 
+            this.kryptonGroup1.Panel.Controls.Add(this.kryptonDataGridView1);
+            this.kryptonGroup1.Panel.Controls.Add(this.btnNegar);
+            this.kryptonGroup1.Panel.Controls.Add(this.btnAutorizar);
+            this.kryptonGroup1.Panel.Controls.Add(this.label2);
+            this.kryptonGroup1.Panel.Controls.Add(this.label1);
+            this.kryptonGroup1.Panel.Controls.Add(this.txtBuscar);
+            this.kryptonGroup1.Size = new System.Drawing.Size(701, 398);
+            this.kryptonGroup1.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(217)))), ((int)(((byte)(220)))), ((int)(((byte)(229)))));
+            this.kryptonGroup1.StateCommon.Border.Rounding = 30F;
+            this.kryptonGroup1.TabIndex = 0;
+            // 
+            // label32
+            // 
+            this.label32.AutoSize = true;
+            this.label32.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label32.ForeColor = System.Drawing.Color.White;
+            this.label32.Location = new System.Drawing.Point(85, 9);
+            this.label32.Name = "label32";
+            this.label32.Size = new System.Drawing.Size(111, 25);
+            this.label32.TabIndex = 2;
+            this.label32.Text = "TMSS.SW";
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(24, 56);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(303, 29);
+            this.txtBuscar.StateActive.Border.Color1 = System.Drawing.Color.Black;
+            this.txtBuscar.StateActive.Border.Color2 = System.Drawing.Color.Black;
+            this.txtBuscar.StateActive.Border.Rounding = 5F;
+            this.txtBuscar.StateActive.Border.Width = 3;
+            this.txtBuscar.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(-4, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(817, 26);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "_________________________________________________________________________________" +
+    "______________________________________________________\r\n\r\n";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Sans Serif Collection", 10.6F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(7, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(359, 51);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "AUTORIZACIÓN DE REGISTROS DE CLIENTES";
+            // 
+            // btnAutorizar
+            // 
+            this.btnAutorizar.Location = new System.Drawing.Point(24, 114);
+            this.btnAutorizar.Name = "btnAutorizar";
+            this.btnAutorizar.Size = new System.Drawing.Size(268, 38);
+            this.btnAutorizar.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(137)))), ((int)(((byte)(19)))));
+            this.btnAutorizar.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(137)))), ((int)(((byte)(19)))));
+            this.btnAutorizar.StateCommon.Border.Color1 = System.Drawing.Color.Black;
+            this.btnAutorizar.StateCommon.Border.Color2 = System.Drawing.Color.Black;
+            this.btnAutorizar.StateCommon.Border.Width = 2;
+            this.btnAutorizar.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.White;
+            this.btnAutorizar.StateCommon.Content.ShortText.Color2 = System.Drawing.Color.White;
+            this.btnAutorizar.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Sans Serif Collection", 9.749998F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAutorizar.TabIndex = 22;
+            this.btnAutorizar.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.btnAutorizar.Values.Text = "AUTORIZAR SELECCIONES";
+            // 
+            // btnNegar
+            // 
+            this.btnNegar.Location = new System.Drawing.Point(311, 114);
+            this.btnNegar.Name = "btnNegar";
+            this.btnNegar.Size = new System.Drawing.Size(268, 38);
+            this.btnNegar.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(28)))), ((int)(((byte)(62)))));
+            this.btnNegar.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(28)))), ((int)(((byte)(62)))));
+            this.btnNegar.StateCommon.Border.Color1 = System.Drawing.Color.Black;
+            this.btnNegar.StateCommon.Border.Color2 = System.Drawing.Color.Black;
+            this.btnNegar.StateCommon.Border.Width = 2;
+            this.btnNegar.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.White;
+            this.btnNegar.StateCommon.Content.ShortText.Color2 = System.Drawing.Color.White;
+            this.btnNegar.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Sans Serif Collection", 9.749998F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNegar.TabIndex = 23;
+            this.btnNegar.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.btnNegar.Values.Text = "DENEGAR  SELECCIONES";
+            // 
+            // kryptonDataGridView1
+            // 
+            this.kryptonDataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.kryptonDataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.kryptonDataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.kryptonDataGridView1.Location = new System.Drawing.Point(38, 158);
+            this.kryptonDataGridView1.Name = "kryptonDataGridView1";
+            this.kryptonDataGridView1.Size = new System.Drawing.Size(546, 229);
+            this.kryptonDataGridView1.StateCommon.Background.Color1 = System.Drawing.Color.White;
+            this.kryptonDataGridView1.StateCommon.BackStyle = Krypton.Toolkit.PaletteBackStyle.GridBackgroundList;
+            this.kryptonDataGridView1.StateCommon.DataCell.Back.Color1 = System.Drawing.Color.White;
+            this.kryptonDataGridView1.StateCommon.DataCell.Back.Color2 = System.Drawing.Color.White;
+            this.kryptonDataGridView1.StateCommon.DataCell.Border.Color1 = System.Drawing.Color.White;
+            this.kryptonDataGridView1.StateCommon.DataCell.Border.Color2 = System.Drawing.Color.White;
+            this.kryptonDataGridView1.StateCommon.HeaderColumn.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(178)))), ((int)(((byte)(178)))));
+            this.kryptonDataGridView1.StateCommon.HeaderColumn.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(178)))), ((int)(((byte)(178)))));
+            this.kryptonDataGridView1.StateCommon.HeaderColumn.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(178)))), ((int)(((byte)(178)))));
+            this.kryptonDataGridView1.StateCommon.HeaderColumn.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(178)))), ((int)(((byte)(178)))), ((int)(((byte)(178)))));
+            this.kryptonDataGridView1.StateCommon.HeaderColumn.Content.Font = new System.Drawing.Font("Segoe MDL2 Assets", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kryptonDataGridView1.StateCommon.HeaderRow.Back.Color1 = System.Drawing.Color.White;
+            this.kryptonDataGridView1.StateCommon.HeaderRow.Back.Color2 = System.Drawing.Color.White;
+            this.kryptonDataGridView1.StateCommon.HeaderRow.Border.Color1 = System.Drawing.Color.White;
+            this.kryptonDataGridView1.StateCommon.HeaderRow.Border.Color2 = System.Drawing.Color.White;
+            this.kryptonDataGridView1.TabIndex = 24;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "ID de Cliente";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Nombre de cliente";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Fecha de Registro";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Tipo de cliente";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Estado";
+            this.Column6.Name = "Column6";
             // 
             // frmAprobacion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(108)))), ((int)(((byte)(71)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label32);
+            this.Controls.Add(this.kryptonGroup1);
             this.Name = "frmAprobacion";
             this.Text = "frmAprobacion";
             this.Load += new System.EventHandler(this.frmAprobacion_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonGroup1.Panel)).EndInit();
+            this.kryptonGroup1.Panel.ResumeLayout(false);
+            this.kryptonGroup1.Panel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonGroup1)).EndInit();
+            this.kryptonGroup1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.kryptonDataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Krypton.Toolkit.KryptonGroup kryptonGroup1;
+        private System.Windows.Forms.Label label32;
+        private Krypton.Toolkit.KryptonTextBox txtBuscar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private Krypton.Toolkit.KryptonButton btnAutorizar;
+        private Krypton.Toolkit.KryptonButton btnNegar;
+        private Krypton.Toolkit.KryptonDataGridView kryptonDataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
     }
 }
