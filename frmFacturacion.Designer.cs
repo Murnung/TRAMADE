e2147fa621b19af1509b4be1b29c60a09f140d6d
Jonathan Avila <Jonathan Avila@DESKTOP-3707B60>
2026-02-21 20:06:55 -0600

    Pruebas

--- frmFacturacion.Designer.cs
+++ frmFacturacion.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmFacturacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "frmFacturacion";
             this.Text = "frmFacturacion";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
