ba8e4f9c055887d4759514cedb7ac1057c33c38c
jorge <jorge@RODAS>
2026-02-22 21:09:03 -0600

    MENU CLIENTE ACTUALIZADO

--- frmMenu.Designer.cs
+++ frmMenu.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.kryptonGroup1 = new Krypton.Toolkit.KryptonGroup();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRegistrar = new Krypton.Toolkit.KryptonButton();
+            this.btnActualizar = new Krypton.Toolkit.KryptonButton();
+            this.btnAutorizar = new Krypton.Toolkit.KryptonButton();
+            this.btnSeguimiento = new Krypton.Toolkit.KryptonButton();
             ((System.ComponentModel.ISupportInitialize)(this.kryptonGroup1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.kryptonGroup1.Panel)).BeginInit();
             this.kryptonGroup1.Panel.SuspendLayout();
@@ -44,7 +49,7 @@
             // 
             this.kryptonGroup1.Panel.Controls.Add(this.label1);
             this.kryptonGroup1.Panel.Enabled = false;
-            this.kryptonGroup1.Size = new System.Drawing.Size(200, 475);
+            this.kryptonGroup1.Size = new System.Drawing.Size(200, 621);
             this.kryptonGroup1.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(108)))), ((int)(((byte)(71)))));
             this.kryptonGroup1.TabIndex = 0;
             // 
@@ -60,12 +65,97 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "TSMS";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Sans Serif Collection", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(202, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(287, 81);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "MÓDULO DE CLIENTES";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(216, 92);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(170, 61);
+            this.btnRegistrar.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(76)))), ((int)(((byte)(58)))));
+            this.btnRegistrar.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(199)))), ((int)(((byte)(76)))), ((int)(((byte)(58)))));
+            this.btnRegistrar.StateCommon.Border.Color1 = System.Drawing.Color.Black;
+            this.btnRegistrar.StateCommon.Border.Color2 = System.Drawing.Color.Black;
+            this.btnRegistrar.StateCommon.Border.Rounding = 15F;
+            this.btnRegistrar.StateCommon.Border.Width = 1;
+            this.btnRegistrar.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.White;
+            this.btnRegistrar.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.TabIndex = 27;
+            this.btnRegistrar.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.btnRegistrar.Values.Text = "REGISTRAR CLIENTE";
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(404, 92);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(170, 61);
+            this.btnActualizar.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(92)))), ((int)(((byte)(104)))));
+            this.btnActualizar.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(92)))), ((int)(((byte)(104)))));
+            this.btnActualizar.StateCommon.Border.Color1 = System.Drawing.Color.Black;
+            this.btnActualizar.StateCommon.Border.Color2 = System.Drawing.Color.Black;
+            this.btnActualizar.StateCommon.Border.Rounding = 15F;
+            this.btnActualizar.StateCommon.Border.Width = 1;
+            this.btnActualizar.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.White;
+            this.btnActualizar.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.TabIndex = 28;
+            this.btnActualizar.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.btnActualizar.Values.Text = "ACTUALIZAR CLIENTE";
+            // 
+            // btnAutorizar
+            // 
+            this.btnAutorizar.Location = new System.Drawing.Point(602, 92);
+            this.btnAutorizar.Name = "btnAutorizar";
+            this.btnAutorizar.Size = new System.Drawing.Size(170, 61);
+            this.btnAutorizar.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(140)))), ((int)(((byte)(92)))));
+            this.btnAutorizar.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(140)))), ((int)(((byte)(92)))));
+            this.btnAutorizar.StateCommon.Border.Color1 = System.Drawing.Color.Black;
+            this.btnAutorizar.StateCommon.Border.Color2 = System.Drawing.Color.Black;
+            this.btnAutorizar.StateCommon.Border.Rounding = 15F;
+            this.btnAutorizar.StateCommon.Border.Width = 1;
+            this.btnAutorizar.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.White;
+            this.btnAutorizar.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAutorizar.TabIndex = 29;
+            this.btnAutorizar.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.btnAutorizar.Values.Text = "AUTORIZAR CLIENTE";
+            // 
+            // btnSeguimiento
+            // 
+            this.btnSeguimiento.Location = new System.Drawing.Point(792, 92);
+            this.btnSeguimiento.Name = "btnSeguimiento";
+            this.btnSeguimiento.Size = new System.Drawing.Size(177, 61);
+            this.btnSeguimiento.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(130)))), ((int)(((byte)(92)))));
+            this.btnSeguimiento.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(130)))), ((int)(((byte)(92)))));
+            this.btnSeguimiento.StateCommon.Border.Color1 = System.Drawing.Color.Black;
+            this.btnSeguimiento.StateCommon.Border.Color2 = System.Drawing.Color.Black;
+            this.btnSeguimiento.StateCommon.Border.Rounding = 15F;
+            this.btnSeguimiento.StateCommon.Border.Width = 1;
+            this.btnSeguimiento.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.White;
+            this.btnSeguimiento.StateCommon.Content.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSeguimiento.TabIndex = 30;
+            this.btnSeguimiento.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.btnSeguimiento.Values.Text = "SEGUIMIENTO DE CLIENTE";
+            // 
             // frmMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1000, 588);
+            this.Controls.Add(this.btnSeguimiento);
+            this.Controls.Add(this.btnAutorizar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.kryptonGroup1);
             this.Name = "frmMenu";
             this.Text = "frmMenu";
@@ -75,6 +165,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.kryptonGroup1)).EndInit();
             this.kryptonGroup1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -82,5 +173,10 @@
 
         private Krypton.Toolkit.KryptonGroup kryptonGroup1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private Krypton.Toolkit.KryptonButton btnRegistrar;
+        private Krypton.Toolkit.KryptonButton btnActualizar;
+        private Krypton.Toolkit.KryptonButton btnAutorizar;
+        private Krypton.Toolkit.KryptonButton btnSeguimiento;
     }
 }
--- frmMenu.cs
+++ frmMenu.cs
@@ -16,5 +16,10 @@ namespace TRAMADE
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
