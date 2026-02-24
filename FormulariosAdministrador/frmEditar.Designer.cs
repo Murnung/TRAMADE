47219825c1a4eecb4d95bd092bcd72d254858a69
vahabond <claudioarh504@gmail.com>
2026-02-23 22:05:48 -0600

    frmEditar

---
+++ FormulariosAdministrador/frmEditar.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace TRAMADE.FormulariosAdministrador
+{
+    partial class frmEditar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbRol = new Krypton.Toolkit.KryptonComboBox();
+            this.btnLimpiar = new Krypton.Toolkit.KryptonButton();
+            this.btnAgregar = new Krypton.Toolkit.KryptonButton();
+            this.cmbSucursal = new Krypton.Toolkit.KryptonComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtContraseña = new Krypton.Toolkit.KryptonTextBox();
+            this.txtNombre = new Krypton.Toolkit.KryptonTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnBuscar = new Krypton.Toolkit.KryptonButton();
+            this.kryptonButton1 = new Krypton.Toolkit.KryptonButton();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbRol)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbSucursal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbRol
+            // 
+            this.cmbRol.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbRol.DropDownWidth = 243;
+            this.cmbRol.Location = new System.Drawing.Point(163, 179);
+            this.cmbRol.Name = "cmbRol";
+            this.cmbRol.Size = new System.Drawing.Size(247, 30);
+            this.cmbRol.StateCommon.ComboBox.Border.Rounding = 5F;
+            this.cmbRol.StateCommon.ComboBox.Content.TextH = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            this.cmbRol.TabIndex = 37;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(309, 311);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.OverrideDefault.Back.Color1 = System.Drawing.SystemColors.AppWorkspace;
+            this.btnLimpiar.OverrideDefault.Back.Color2 = System.Drawing.SystemColors.AppWorkspace;
+            this.btnLimpiar.OverrideFocus.Back.Color1 = System.Drawing.SystemColors.AppWorkspace;
+            this.btnLimpiar.OverrideFocus.Back.Color2 = System.Drawing.SystemColors.ButtonFace;
+            this.btnLimpiar.Size = new System.Drawing.Size(113, 34);
+            this.btnLimpiar.StateCommon.Back.Color1 = System.Drawing.SystemColors.AppWorkspace;
+            this.btnLimpiar.StateCommon.Back.Color2 = System.Drawing.SystemColors.ButtonShadow;
+            this.btnLimpiar.StateCommon.Border.Color1 = System.Drawing.SystemColors.AppWorkspace;
+            this.btnLimpiar.StateCommon.Border.Color2 = System.Drawing.SystemColors.AppWorkspace;
+            this.btnLimpiar.StateCommon.Border.Rounding = 10F;
+            this.btnLimpiar.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.Black;
+            this.btnLimpiar.StateTracking.Back.Color1 = System.Drawing.SystemColors.AppWorkspace;
+            this.btnLimpiar.StateTracking.Back.Color2 = System.Drawing.SystemColors.ButtonFace;
+            this.btnLimpiar.StateTracking.Back.ColorStyle = Krypton.Toolkit.PaletteColorStyle.ExpertCheckedTracking;
+            this.btnLimpiar.StateTracking.Content.ShortText.Color1 = System.Drawing.SystemColors.ButtonFace;
+            this.btnLimpiar.StateTracking.Content.ShortText.Color2 = System.Drawing.SystemColors.ButtonFace;
+            this.btnLimpiar.TabIndex = 36;
+            this.btnLimpiar.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.btnLimpiar.Values.Text = "LIMPIAR";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(7, 311);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.OverrideDefault.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(198)))), ((int)(((byte)(224)))));
+            this.btnAgregar.OverrideDefault.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(198)))), ((int)(((byte)(224)))));
+            this.btnAgregar.OverrideFocus.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(198)))), ((int)(((byte)(224)))));
+            this.btnAgregar.OverrideFocus.Back.Color2 = System.Drawing.Color.LightBlue;
+            this.btnAgregar.Size = new System.Drawing.Size(174, 34);
+            this.btnAgregar.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(198)))), ((int)(((byte)(224)))));
+            this.btnAgregar.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(198)))), ((int)(((byte)(224)))));
+            this.btnAgregar.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(64)))), ((int)(((byte)(137)))));
+            this.btnAgregar.StateCommon.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(64)))), ((int)(((byte)(137)))));
+            this.btnAgregar.StateCommon.Border.Rounding = 10F;
+            this.btnAgregar.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.Black;
+            this.btnAgregar.StateTracking.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(64)))), ((int)(((byte)(137)))));
+            this.btnAgregar.StateTracking.Back.Color2 = System.Drawing.Color.LightBlue;
+            this.btnAgregar.StateTracking.Back.ColorStyle = Krypton.Toolkit.PaletteColorStyle.ExpertCheckedTracking;
+            this.btnAgregar.StateTracking.Content.ShortText.Color1 = System.Drawing.SystemColors.ButtonFace;
+            this.btnAgregar.StateTracking.Content.ShortText.Color2 = System.Drawing.SystemColors.ButtonFace;
+            this.btnAgregar.TabIndex = 35;
+            this.btnAgregar.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.btnAgregar.Values.Text = "CONFIRMAR CAMBIOS";
+            // 
+            // cmbSucursal
+            // 
+            this.cmbSucursal.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbSucursal.DropDownWidth = 243;
+            this.cmbSucursal.Location = new System.Drawing.Point(163, 241);
+            this.cmbSucursal.Name = "cmbSucursal";
+            this.cmbSucursal.Size = new System.Drawing.Size(247, 30);
+            this.cmbSucursal.StateCommon.ComboBox.Border.Rounding = 5F;
+            this.cmbSucursal.StateCommon.ComboBox.Content.TextH = Krypton.Toolkit.PaletteRelativeAlign.Near;
+            this.cmbSucursal.TabIndex = 34;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(14, 20);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(396, 32);
+            this.label7.TabIndex = 33;
+            this.label7.Text = "AGREGAR NUEVO USUARIO";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(17, 241);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 18);
+            this.label6.TabIndex = 32;
+            this.label6.Text = "Sucursal:";
+            // 
+            // txtContraseña
+            // 
+            this.txtContraseña.Location = new System.Drawing.Point(163, 122);
+            this.txtContraseña.Name = "txtContraseña";
+            this.txtContraseña.Size = new System.Drawing.Size(247, 31);
+            this.txtContraseña.StateCommon.Border.Rounding = 5F;
+            this.txtContraseña.TabIndex = 31;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(163, 77);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(247, 31);
+            this.txtNombre.StateCommon.Border.Rounding = 5F;
+            this.txtNombre.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 18);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Nombre/Correo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(17, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 18);
+            this.label4.TabIndex = 27;
+            this.label4.Text = "Contraseña:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(17, 179);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 18);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "Rol:";
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(416, 77);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.OverrideDefault.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(198)))), ((int)(((byte)(224)))));
+            this.btnBuscar.OverrideDefault.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(198)))), ((int)(((byte)(224)))));
+            this.btnBuscar.OverrideFocus.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(198)))), ((int)(((byte)(224)))));
+            this.btnBuscar.OverrideFocus.Back.Color2 = System.Drawing.Color.LightBlue;
+            this.btnBuscar.Size = new System.Drawing.Size(103, 25);
+            this.btnBuscar.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(198)))), ((int)(((byte)(224)))));
+            this.btnBuscar.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(111)))), ((int)(((byte)(198)))), ((int)(((byte)(224)))));
+            this.btnBuscar.StateCommon.Border.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(64)))), ((int)(((byte)(137)))));
+            this.btnBuscar.StateCommon.Border.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(64)))), ((int)(((byte)(137)))));
+            this.btnBuscar.StateCommon.Border.Rounding = 10F;
+            this.btnBuscar.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.Black;
+            this.btnBuscar.StateTracking.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(64)))), ((int)(((byte)(137)))));
+            this.btnBuscar.StateTracking.Back.Color2 = System.Drawing.Color.LightBlue;
+            this.btnBuscar.StateTracking.Back.ColorStyle = Krypton.Toolkit.PaletteColorStyle.ExpertCheckedTracking;
+            this.btnBuscar.StateTracking.Content.ShortText.Color1 = System.Drawing.SystemColors.ButtonFace;
+            this.btnBuscar.StateTracking.Content.ShortText.Color2 = System.Drawing.SystemColors.ButtonFace;
+            this.btnBuscar.TabIndex = 40;
+            this.btnBuscar.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.btnBuscar.Values.Text = "BUSCAR";
+            // 
+            // kryptonButton1
+            // 
+            this.kryptonButton1.Location = new System.Drawing.Point(190, 311);
+            this.kryptonButton1.Name = "kryptonButton1";
+            this.kryptonButton1.OverrideDefault.Back.Color1 = System.Drawing.SystemColors.AppWorkspace;
+            this.kryptonButton1.OverrideDefault.Back.Color2 = System.Drawing.SystemColors.AppWorkspace;
+            this.kryptonButton1.OverrideFocus.Back.Color1 = System.Drawing.SystemColors.AppWorkspace;
+            this.kryptonButton1.OverrideFocus.Back.Color2 = System.Drawing.SystemColors.ButtonFace;
+            this.kryptonButton1.Size = new System.Drawing.Size(113, 34);
+            this.kryptonButton1.StateCommon.Back.Color1 = System.Drawing.SystemColors.AppWorkspace;
+            this.kryptonButton1.StateCommon.Back.Color2 = System.Drawing.SystemColors.ButtonShadow;
+            this.kryptonButton1.StateCommon.Border.Color1 = System.Drawing.SystemColors.AppWorkspace;
+            this.kryptonButton1.StateCommon.Border.Color2 = System.Drawing.SystemColors.AppWorkspace;
+            this.kryptonButton1.StateCommon.Border.Rounding = 10F;
+            this.kryptonButton1.StateCommon.Content.ShortText.Color1 = System.Drawing.Color.Black;
+            this.kryptonButton1.StateTracking.Back.Color1 = System.Drawing.SystemColors.AppWorkspace;
+            this.kryptonButton1.StateTracking.Back.Color2 = System.Drawing.SystemColors.ButtonFace;
+            this.kryptonButton1.StateTracking.Back.ColorStyle = Krypton.Toolkit.PaletteColorStyle.ExpertCheckedTracking;
+            this.kryptonButton1.StateTracking.Content.ShortText.Color1 = System.Drawing.SystemColors.ButtonFace;
+            this.kryptonButton1.StateTracking.Content.ShortText.Color2 = System.Drawing.SystemColors.ButtonFace;
+            this.kryptonButton1.TabIndex = 41;
+            this.kryptonButton1.Values.DropDownArrowColor = System.Drawing.Color.Empty;
+            this.kryptonButton1.Values.Text = "CANCELAR";
+            // 
+            // frmEditar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 372);
+            this.Controls.Add(this.kryptonButton1);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.cmbRol);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.cmbSucursal);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtContraseña);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Name = "frmEditar";
+            this.Text = "frmEditar";
+            ((System.ComponentModel.ISupportInitialize)(this.cmbRol)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbSucursal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Krypton.Toolkit.KryptonComboBox cmbRol;
+        private Krypton.Toolkit.KryptonButton btnLimpiar;
+        private Krypton.Toolkit.KryptonButton btnAgregar;
+        private Krypton.Toolkit.KryptonComboBox cmbSucursal;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private Krypton.Toolkit.KryptonTextBox txtContraseña;
+        private Krypton.Toolkit.KryptonTextBox txtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private Krypton.Toolkit.KryptonButton btnBuscar;
+        private Krypton.Toolkit.KryptonButton kryptonButton1;
+    }
+}
