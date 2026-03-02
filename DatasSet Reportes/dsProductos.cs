12236770ee51f3d27b4699888f7b92fe72648e84
Anthony <zadkielr614@gmail.com>
2026-03-02 09:55:15 -0600

    jojo

---
+++ DatasSet Reportes/dsProductos.cs
@@ -0,0 +1,38 @@
+﻿namespace TRAMADE.DatasSet_Reportes
+{
+
+
+    partial class dsProductos
+    {
+        partial class DataTable2DataTable
+        {
+        }
+    }
+}
+
+namespace TRAMADE.DatasSet_Reportes.dsProductosTableAdapters
+{
+
+
+    public partial class DataTable1TableAdapter
+    {
+    }
+}
+
+namespace TRAMADE.DatasSet_Reportes.dsProductosTableAdapters
+{
+
+
+    public partial class DataTable1TableAdapter
+    {
+    }
+}
+
+namespace TRAMADE.DatasSet_Reportes.dsProductosTableAdapters
+{
+
+
+    public partial class DataTable1TableAdapter
+    {
+    }
+}
