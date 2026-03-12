64c683992db7f6fc737dd3d864f50135a36ef9d2
johannncito <johanfloresmejia2008@gmail.com>
2026-03-12 13:44:09 -0600

    50% de validaciones de inventario

--- FormulariosInventario/frmInventario.Designer.cs
+++ FormulariosInventario/frmInventario.Designer.cs
@@ -63,10 +63,9 @@
             // txtBuscar
             // 
             this.txtBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(212)))));
-            this.txtBuscar.Location = new System.Drawing.Point(65, 20);
-            this.txtBuscar.Margin = new System.Windows.Forms.Padding(2);
+            this.txtBuscar.Location = new System.Drawing.Point(87, 25);
             this.txtBuscar.Name = "txtBuscar";
-            this.txtBuscar.Size = new System.Drawing.Size(146, 20);
+            this.txtBuscar.Size = new System.Drawing.Size(193, 22);
             this.txtBuscar.TabIndex = 0;
             this.txtBuscar.TextChanged += new System.EventHandler(this.txtBuscar_TextChanged);
             // 
@@ -75,20 +74,18 @@
             this.cmbFiltrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(212)))));
             this.cmbFiltrar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbFiltrar.FormattingEnabled = true;
-            this.cmbFiltrar.Location = new System.Drawing.Point(326, 22);
-            this.cmbFiltrar.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbFiltrar.Location = new System.Drawing.Point(434, 27);
             this.cmbFiltrar.Name = "cmbFiltrar";
-            this.cmbFiltrar.Size = new System.Drawing.Size(129, 21);
+            this.cmbFiltrar.Size = new System.Drawing.Size(171, 24);
             this.cmbFiltrar.TabIndex = 1;
             this.cmbFiltrar.SelectedIndexChanged += new System.EventHandler(this.cmbFiltrar_SelectedIndexChanged);
             // 
             // btnRegistrar
             // 
             this.btnRegistrar.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnRegistrar.Location = new System.Drawing.Point(529, 21);
-            this.btnRegistrar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRegistrar.Location = new System.Drawing.Point(705, 26);
             this.btnRegistrar.Name = "btnRegistrar";
-            this.btnRegistrar.Size = new System.Drawing.Size(61, 20);
+            this.btnRegistrar.Size = new System.Drawing.Size(81, 25);
             this.btnRegistrar.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(212)))));
             this.btnRegistrar.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(212)))));
             this.btnRegistrar.TabIndex = 2;
@@ -99,10 +96,9 @@
             // btnEditar
             // 
             this.btnEditar.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnEditar.Location = new System.Drawing.Point(608, 21);
-            this.btnEditar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEditar.Location = new System.Drawing.Point(811, 26);
             this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(61, 20);
+            this.btnEditar.Size = new System.Drawing.Size(81, 25);
             this.btnEditar.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(212)))));
             this.btnEditar.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(212)))));
             this.btnEditar.TabIndex = 3;
@@ -113,10 +109,9 @@
             // btnEliminar
             // 
             this.btnEliminar.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnEliminar.Location = new System.Drawing.Point(686, 21);
-            this.btnEliminar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnEliminar.Location = new System.Drawing.Point(915, 26);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(61, 20);
+            this.btnEliminar.Size = new System.Drawing.Size(81, 25);
             this.btnEliminar.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
             this.btnEliminar.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
             this.btnEliminar.TabIndex = 4;
@@ -128,10 +123,9 @@
             // 
             this.lblFecha.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.lblFecha.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblFecha.Location = new System.Drawing.Point(870, 11);
-            this.lblFecha.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblFecha.Location = new System.Drawing.Point(1160, 14);
             this.lblFecha.Name = "lblFecha";
-            this.lblFecha.Size = new System.Drawing.Size(75, 19);
+            this.lblFecha.Size = new System.Drawing.Size(100, 23);
             this.lblFecha.TabIndex = 5;
             this.lblFecha.Text = "Fecha";
             this.lblFecha.Click += new System.EventHandler(this.lblFecha_Click);
@@ -139,10 +133,9 @@
             // btnRegresar
             // 
             this.btnRegresar.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnRegresar.Location = new System.Drawing.Point(65, 382);
-            this.btnRegresar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRegresar.Location = new System.Drawing.Point(50, 484);
             this.btnRegresar.Name = "btnRegresar";
-            this.btnRegresar.Size = new System.Drawing.Size(61, 20);
+            this.btnRegresar.Size = new System.Drawing.Size(81, 25);
             this.btnRegresar.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(212)))));
             this.btnRegresar.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(212)))));
             this.btnRegresar.TabIndex = 7;
@@ -152,10 +145,9 @@
             // btnExportarInventario
             // 
             this.btnExportarInventario.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btnExportarInventario.Location = new System.Drawing.Point(616, 382);
-            this.btnExportarInventario.Margin = new System.Windows.Forms.Padding(2);
+            this.btnExportarInventario.Location = new System.Drawing.Point(858, 485);
             this.btnExportarInventario.Name = "btnExportarInventario";
-            this.btnExportarInventario.Size = new System.Drawing.Size(131, 20);
+            this.btnExportarInventario.Size = new System.Drawing.Size(175, 25);
             this.btnExportarInventario.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(212)))));
             this.btnExportarInventario.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(212)))));
             this.btnExportarInventario.TabIndex = 8;
@@ -166,34 +158,31 @@
             // lblPagina
             // 
             this.lblPagina.AutoSize = true;
-            this.lblPagina.Location = new System.Drawing.Point(824, 384);
-            this.lblPagina.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPagina.Location = new System.Drawing.Point(1098, 488);
             this.lblPagina.Name = "lblPagina";
-            this.lblPagina.Size = new System.Drawing.Size(73, 13);
+            this.lblPagina.Size = new System.Drawing.Size(89, 16);
             this.lblPagina.TabIndex = 9;
             this.lblPagina.Text = "Página 1 de 2";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(654, 329);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(872, 405);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.Size = new System.Drawing.Size(0, 16);
             this.label2.TabIndex = 10;
             // 
             // dgvInventario
             // 
             this.dgvInventario.AllowUserToAddRows = false;
             this.dgvInventario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvInventario.Location = new System.Drawing.Point(18, 59);
-            this.dgvInventario.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvInventario.Location = new System.Drawing.Point(24, 73);
             this.dgvInventario.Name = "dgvInventario";
             this.dgvInventario.RowHeadersVisible = false;
             this.dgvInventario.RowHeadersWidth = 51;
             this.dgvInventario.RowTemplate.Height = 24;
             this.dgvInventario.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvInventario.Size = new System.Drawing.Size(920, 266);
+            this.dgvInventario.Size = new System.Drawing.Size(1227, 327);
             this.dgvInventario.TabIndex = 12;
             this.dgvInventario.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvInventario_CellContentClick);
             this.dgvInventario.SelectionChanged += new System.EventHandler(this.dgvInventario_SelectionChanged);
@@ -203,20 +192,18 @@
             this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(212)))));
             this.panel2.Controls.Add(this.imgProducto);
             this.panel2.Controls.Add(this.lblNombreProducto);
-            this.panel2.Location = new System.Drawing.Point(182, 341);
-            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Location = new System.Drawing.Point(242, 420);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(348, 84);
+            this.panel2.Size = new System.Drawing.Size(464, 104);
             this.panel2.TabIndex = 13;
             this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // imgProducto
             // 
             this.imgProducto.Image = global::TRAMADE.Properties.Resources.photo_8924441;
-            this.imgProducto.Location = new System.Drawing.Point(24, 6);
-            this.imgProducto.Margin = new System.Windows.Forms.Padding(2);
+            this.imgProducto.Location = new System.Drawing.Point(32, 8);
             this.imgProducto.Name = "imgProducto";
-            this.imgProducto.Size = new System.Drawing.Size(106, 74);
+            this.imgProducto.Size = new System.Drawing.Size(141, 91);
             this.imgProducto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.imgProducto.TabIndex = 1;
             this.imgProducto.TabStop = false;
@@ -226,19 +213,17 @@
             this.lblNombreProducto.AutoSize = true;
             this.lblNombreProducto.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblNombreProducto.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(141)))), ((int)(((byte)(144)))));
-            this.lblNombreProducto.Location = new System.Drawing.Point(140, 23);
-            this.lblNombreProducto.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblNombreProducto.Location = new System.Drawing.Point(186, 28);
             this.lblNombreProducto.Name = "lblNombreProducto";
-            this.lblNombreProducto.Size = new System.Drawing.Size(102, 38);
+            this.lblNombreProducto.Size = new System.Drawing.Size(122, 46);
             this.lblNombreProducto.TabIndex = 0;
             this.lblNombreProducto.Text = "Imagen \r\ndel Producto";
             // 
             // btnSiguiente
             // 
-            this.btnSiguiente.Location = new System.Drawing.Point(895, 382);
-            this.btnSiguiente.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSiguiente.Location = new System.Drawing.Point(1193, 485);
             this.btnSiguiente.Name = "btnSiguiente";
-            this.btnSiguiente.Size = new System.Drawing.Size(21, 20);
+            this.btnSiguiente.Size = new System.Drawing.Size(28, 25);
             this.btnSiguiente.StateCommon.Back.Color1 = System.Drawing.SystemColors.Control;
             this.btnSiguiente.StateCommon.Back.Color2 = System.Drawing.SystemColors.Control;
             this.btnSiguiente.TabIndex = 16;
@@ -249,10 +234,9 @@
             // 
             // btnAtras
             // 
-            this.btnAtras.Location = new System.Drawing.Point(799, 381);
-            this.btnAtras.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAtras.Location = new System.Drawing.Point(1065, 484);
             this.btnAtras.Name = "btnAtras";
-            this.btnAtras.Size = new System.Drawing.Size(20, 20);
+            this.btnAtras.Size = new System.Drawing.Size(27, 25);
             this.btnAtras.StateCommon.Back.Color1 = System.Drawing.SystemColors.Control;
             this.btnAtras.StateCommon.Back.Color2 = System.Drawing.SystemColors.Control;
             this.btnAtras.TabIndex = 17;
@@ -264,20 +248,18 @@
             // pictureBox3
             // 
             this.pictureBox3.Image = global::TRAMADE.Properties.Resources.calendar_55281;
-            this.pictureBox3.Location = new System.Drawing.Point(810, 19);
-            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Location = new System.Drawing.Point(1080, 23);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(27, 20);
+            this.pictureBox3.Size = new System.Drawing.Size(36, 25);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 15;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(578, 70);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Location = new System.Drawing.Point(771, 86);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(75, 41);
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
@@ -285,19 +267,17 @@
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(148)))), ((int)(((byte)(114)))), ((int)(((byte)(71)))));
             this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Location = new System.Drawing.Point(0, 133);
-            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Location = new System.Drawing.Point(0, 164);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(967, 150);
+            this.panel1.Size = new System.Drawing.Size(1289, 185);
             this.panel1.TabIndex = 11;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(220, 20);
-            this.btnBuscar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnBuscar.Location = new System.Drawing.Point(293, 25);
             this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(20, 20);
+            this.btnBuscar.Size = new System.Drawing.Size(27, 25);
             this.btnBuscar.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(212)))));
             this.btnBuscar.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(212)))));
             this.btnBuscar.TabIndex = 18;
@@ -308,10 +288,9 @@
             // 
             // btnRefrescar
             // 
-            this.btnRefrescar.Location = new System.Drawing.Point(249, 20);
-            this.btnRefrescar.Margin = new System.Windows.Forms.Padding(2);
+            this.btnRefrescar.Location = new System.Drawing.Point(332, 25);
             this.btnRefrescar.Name = "btnRefrescar";
-            this.btnRefrescar.Size = new System.Drawing.Size(20, 20);
+            this.btnRefrescar.Size = new System.Drawing.Size(27, 25);
             this.btnRefrescar.StateCommon.Back.Color1 = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(212)))));
             this.btnRefrescar.StateCommon.Back.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(210)))), ((int)(((byte)(212)))));
             this.btnRefrescar.TabIndex = 19;
@@ -324,10 +303,9 @@
             // 
             this.lblHora.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.lblHora.Font = new System.Drawing.Font("MS Reference Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHora.Location = new System.Drawing.Point(870, 30);
-            this.lblHora.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblHora.Location = new System.Drawing.Point(1160, 37);
             this.lblHora.Name = "lblHora";
-            this.lblHora.Size = new System.Drawing.Size(86, 19);
+            this.lblHora.Size = new System.Drawing.Size(115, 23);
             this.lblHora.TabIndex = 20;
             this.lblHora.Text = "Hora";
             this.lblHora.Click += new System.EventHandler(this.lblHora_Click);
@@ -339,10 +317,9 @@
             // 
             // frmInventario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(963, 436);
-            this.ControlBox = false;
+            this.ClientSize = new System.Drawing.Size(1284, 536);
             this.Controls.Add(this.lblHora);
             this.Controls.Add(this.btnRefrescar);
             this.Controls.Add(this.btnBuscar);
@@ -362,9 +339,9 @@
             this.Controls.Add(this.btnRegistrar);
             this.Controls.Add(this.cmbFiltrar);
             this.Controls.Add(this.txtBuscar);
-            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmInventario";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmInventario";
             this.Load += new System.EventHandler(this.frmInventario_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvInventario)).EndInit();
             this.panel2.ResumeLayout(false);
