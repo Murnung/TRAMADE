54d16eb2480537386fe8b161b734b019d63b518e
orlanzr18-cmyk <orlanzr18@gmail.com>
2026-03-20 15:34:48 -0600

    borrado de salir sin referencia

--- FormulariosProveedor/frmProveedores_Perfil.cs
+++ FormulariosProveedor/frmProveedores_Perfil.cs
@@ -36,10 +36,5 @@ namespace TRAMADE.Formulario_Proveedores
             frmProveedores_Editar frmEditar = new frmProveedores_Editar(_idProveedor);
             frmEditar.ShowDialog();
         }
-
-        private void btnSalir_Click(object sender, EventArgs e)
-        {
-            this.Close();
-        }
     }
 }
